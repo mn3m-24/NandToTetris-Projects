c796856078c74902b3f63afecf278c56359e3030
abdalmonem <112807908+abdalmonem-24@users.noreply.github.com>
2025-07-04 04:34:07 +0300

    Add Project 2 Solution

---
+++ 02/add16.py
@@ -0,0 +1,2 @@
+for i in range(1, 16):
+    print(f"FullAdder (a=a[{i}], b=b[{i}], c=cout{i - 1}, sum=out[{i}], carry=cout{i});")
---
+++ 02/inc16.py
@@ -0,0 +1,2 @@
+for i in range(1, 16):
+    print(f"HalfAdder (a=in[{i}], b=cout{i - 1}, sum=out[{i}], carry=cout{i});")
